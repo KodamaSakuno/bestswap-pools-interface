5470012db55fb2fbd37e83e286cd1e36e4200718
Garfield Lee <openpgp@550.moe>
2020-10-16 22:20:07 +0800

    fix: fix some ui bugs

--- src/components/TopBar/components/Nav.tsx
+++ src/components/TopBar/components/Nav.tsx
@@ -57,23 +57,23 @@ const StyledLink = styled(NavLink)`
   }
 `
 
-const StyledAbsoluteLink = styled.a`
-  color: ${(props) => props.theme.color.yellow};
-  font-weight: normal;
-  padding-left: ${(props) => props.theme.spacing[3]}px;
-  padding-right: ${(props) => props.theme.spacing[3]}px;
-  text-decoration: none;
-  &:hover {
-    color: ${(props) => props.theme.color.yellow};
-  }
-  &.active {
-    color: ${(props) => props.theme.color.yellow};
-    font-weight: bold;
-  }
-  @media (max-width: 400px) {
-    padding-left: ${(props) => props.theme.spacing[2]}px;
-    padding-right: ${(props) => props.theme.spacing[2]}px;
-  }
-`
+// const StyledAbsoluteLink = styled.a`
+//   color: ${(props) => props.theme.color.yellow};
+//   font-weight: normal;
+//   padding-left: ${(props) => props.theme.spacing[3]}px;
+//   padding-right: ${(props) => props.theme.spacing[3]}px;
+//   text-decoration: none;
+//   &:hover {
+//     color: ${(props) => props.theme.color.yellow};
+//   }
+//   &.active {
+//     color: ${(props) => props.theme.color.yellow};
+//     font-weight: bold;
+//   }
+//   @media (max-width: 400px) {
+//     padding-left: ${(props) => props.theme.spacing[2]}px;
+//     padding-right: ${(props) => props.theme.spacing[2]}px;
+//   }
+// `
 
 export default Nav
